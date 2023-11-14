42ee913269b65fc55185a28e48ba177320f8ceb2
Seitova Ralina <seitovaralina1@gmail.com>
2023-11-14 08:46:13 +0600

    Generate experience

--- CharacterGeneration/GeneratePlayer/Generate.cs
+++ CharacterGeneration/GeneratePlayer/Generate.cs
@@ -10,6 +10,7 @@ public class Generate
 {
     private int count_people;
     private Dictionary<string, string[]> body_type = new Dictionary<string, string[]>();
+    private Dictionary<string, int[]> experience = new Dictionary<string, int[]>();
     private Random rand = new Random();
     public Generate(int count_people)
     {
@@ -27,13 +28,26 @@ public class Generate
             body_type.Add(attribute, value);
         }
     }
+    private void Experience_Info()
+    {
+        string filePath = "Bunker/Data/body_type.txt";
+        string[] lines = File.ReadAllLines(filePath);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(';');
+            // разделять стаж работы на массив int???
+        }
+    }
 
     public Player[] GeneratePlayers()
     {
         Player[] players = new Player[count_people];
+        Body_Type_Info();
         for (int i=0; i < count_people; i++)
         {
-            players[i] = new Player() { Body = body_type.ElementAt(rand.Next(0, body_type.Count))};
+            players[i] = new Player() { 
+                Body = body_type.ElementAt(rand.Next(0, body_type.Count))
+            };
         }
         return players;
     }
